cdd58f149fc71acc3e14573e234cd745fc1db532
mnomanmnasir <samariatajamul563@gmail.com>
2025-12-24 16:08:22 +0500

    Update axiosInterceptor.js
    add

--- FRONTEND/src/utils/axiosInterceptor.js
+++ FRONTEND/src/utils/axiosInterceptor.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://nishat-api.vercel.app";
 
 // Create an axios instance
 const axiosInstance = axios.create({
